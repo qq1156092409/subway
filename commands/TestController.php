0a60c042917f5d3335e05d637b27789fd8d67409
qq1156092409 <1156092409@qq.com>
2016-09-12 23:20:23 +0800

    verdor

--- commands/TestController.php
+++ commands/TestController.php
@@ -4,6 +4,7 @@ namespace app\commands;
 
 
 use app\extensions\custom\taobao\TopClient;
+use Pheanstalk\Pheanstalk;
 use yii\console\Controller;
 
 class TestController extends Controller {
@@ -14,6 +15,14 @@ class TestController extends Controller {
         $request->setNick($nick);
         $response=$client->execute($request,"6202514834d8ac34b57827ZZ735ZZe0dd2be2a841720e60348736849");
         print_r($response);
+    }
+    public function actionQueue(){
+        $pheanstalk = new Pheanstalk('127.0.0.1');
+
+        $job = $pheanstalk ->watch('tubeName') ->ignore('default') ->reserve();
+        $time=$job->getData();
+        echo date("Y-m-d H:i:s",$time);
+        $pheanstalk->delete($job);
     }
 
 }
