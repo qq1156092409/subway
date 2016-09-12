e9752c204e52f56760faa107c8637cf690947973
qq1156092409 <1156092409@qq.com>
2016-09-12 21:46:03 +0800

    queue

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace app\controllers;
+use Pheanstalk\Pheanstalk;
 use yii\web\Controller;
 use app\extensions\custom\taobao\TopClient;
 
@@ -12,5 +13,9 @@ class TestController extends Controller {
         $request->setNick($nick);
         $response=$client->execute($request,"6202514834d8ac34b57827ZZ735ZZe0dd2be2a841720e60348736849");
         print_r($response);
+    }
+    public function actionQueueAdd(){
+        $pheanstalk = new Pheanstalk('120.25.240.36');
+        $pheanstalk->useTube('testtube')->put(time());
     }
 }
