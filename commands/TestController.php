014920b0dc38dd9b42029b039f645b9afaf16dbb
dengjq <1156092409@qq.com>
2016-09-23 17:26:28 +0800

    refresh data

--- commands/TestController.php
+++ commands/TestController.php
@@ -4,17 +4,15 @@ namespace app\commands;
 
 
 use app\extensions\custom\taobao\TopClient;
+use app\models\Balance;
+use app\models\execute\BalanceExecute;
+use app\models\Store;
 use Pheanstalk\Pheanstalk;
 use yii\console\Controller;
 
 class TestController extends Controller {
     public function actionIndex(){
-        $nick="saxcy09";
-        $client = TopClient::getInstance();
-        $request=new \SimbaAccountBalanceGetRequest();
-        $request->setNick($nick);
-        $response=$client->execute($request,"6202514834d8ac34b57827ZZ735ZZe0dd2be2a841720e60348736849");
-        print_r($response);
+
     }
     public function actionQueue(){
         $pheanstalk = new Pheanstalk('127.0.0.1');
---
+++ models/execute/StoreExecute.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2016/9/22
+ * Time: 23:00
+ */
+
+namespace app\models\execute;
+
+
+class StoreExecute
+{
+
+}
